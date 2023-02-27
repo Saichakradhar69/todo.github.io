40fcff70f893ed53810a4315136551f358126c9a
Sai Chakradhar <97936188+Saichakradhar69@users.noreply.github.com>
2023-02-27 22:11:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,30 @@
-// Select the form, input, and list elements
 const form = document.querySelector('form');
 const input = document.querySelector('#new-task');
 const list = document.querySelector('#task-list');
-
-// Add a submit event listener to the form
 form.addEventListener('submit', (event) => {
-  event.preventDefault(); // Prevent the form from submitting and refreshing the page
-  const task = input.value.trim(); // Get the trimmed value of the input
+  event.preventDefault();
+  const task = input.value.trim();
   if (task) {
-    addTask(task); // If the task is not empty, add it to the list
-    input.value = ''; // Clear the input
+    addTask(task);
+    input.value = '';
   }
 });
 
-// Add a new task to the list
 function addTask(task) {
-  const li = document.createElement('li'); // Create a new list item element
-  li.textContent = task; // Set the text content of the list item to the task
-  const checkbox = document.createElement('input'); // Create a new checkbox input element
-  checkbox.type = 'checkbox'; // Set the type of the checkbox to "checkbox"
-  checkbox.addEventListener('change', () => { // Add a change event listener to the checkbox
-    li.classList.toggle('completed'); // Toggle the "completed" class on the list item when the checkbox is changed
+  const li = document.createElement('li');
+  li.textContent = task;
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.addEventListener('change', () => {
+    li.classList.toggle('completed');
   });
-  const deleteButton = document.createElement('button'); // Create a new button element
-  deleteButton.classList.add('delete'); // Add the "delete" class to the button
-  deleteButton.innerHTML = '&times;'; // Set the inner HTML of the button to an "x" symbol
-  deleteButton.addEventListener('click', () => { // Add a click event listener to the button
-    li.remove(); // Remove the list item when the button is clicked
+  const deleteButton = document.createElement('button');
+  deleteButton.classList.add('delete');
+  deleteButton.innerHTML = '&times;';
+  deleteButton.addEventListener('click', () => {
+    li.remove();
   });
-  li.appendChild(checkbox); // Add the checkbox to the list item
-  li.appendChild(deleteButton); // Add the delete button to the list item
-  list.appendChild(li); // Add the list item to the list
+  li.appendChild(checkbox);
+  li.appendChild(deleteButton);
+  list.appendChild(li);
 }
